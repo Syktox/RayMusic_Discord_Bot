328478ef23e4b918fc53a55e9b65b5ce01416de6
syktox <markus.kammerstetter@hotmail.com>
2024-11-23 19:28:07 +0700

    Added some messages

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
