af6e36bed0cded7080640017e0a570e8f8d6c398
syktox <markus.kammerstetter@hotmail.com>
2024-11-23 22:26:17 +0700

    Bot can join a channel now

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def save_message(message):
     file.close()
 
 intents = discord.Intents.default()
+intents.voice_states=True
 intents.message_content=True
-
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
@@ -23,8 +23,9 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$play '):
-        save_message(message)
+    save_message(message)
+
+    if message.content.startswith('$play'):
         await message.channel.send('Hello!')
 
     if message.content.startswith('$pause'):
@@ -38,6 +39,33 @@ async def on_message(message):
 
     if message.content.startswith('$stop'):
         pass
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            try:
+                await channel.connect()
+                await ctx.send(f'Joined {channel.name}!')
+            except discord.ClientException as e:
+                await ctx.send(f"Error: {e}")
+        else:
+            await ctx.send("I'm already connected to a voice channel!")
+    else:
+        await ctx.send("You need to be in a voice channel for me to join!")
+
+
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnected from the voice channel.")
+    else:
+        await ctx.send("I'm not connected to a voice channel!")
+
 
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
