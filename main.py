6584a1a8884294cf2f772a01d4ab2280f4ed8408
syktox <markus.kammerstetter@hotmail.com>
2024-11-23 21:25:15 +0700

    added a log for the messages, yes i am diabolic

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
+def save_message(message):
+    file = open('log.txt', 'a')
+    file.write(f'{message.author} {message.content}\n')
+    file.close()
+
 intents = discord.Intents.default()
 intents.message_content=True
 
@@ -18,12 +23,21 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('$play '):
+        save_message(message)
         await message.channel.send('Hello!')
 
-@bot.command()
-async def test(ctx: commands.Context):
-    await ctx.send("test")
+    if message.content.startswith('$pause'):
+        pass
+
+    if message.content.startswith('$resume'):
+        pass
+
+    if message.content.startswith('$skip'):
+        pass
+
+    if message.content.startswith('$stop'):
+        pass
 
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
