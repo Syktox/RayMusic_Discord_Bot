e2af3cbf687e5b2fcb5ec7f651ec0f2d4a9ea3b3
syktox <markus.kammerstetter@hotmail.com>
2025-01-16 20:15:09 +0100

    idk if it works

--- BotCommands.py
+++ BotCommands.py
@@ -1,51 +1,52 @@
 import discord
 from discord.ext import commands
-import youtube_dl
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import asyncio
-import os
+import yt_dlp
 
-# Configure Spotify API
-SPOTIFY_CLIENT_ID = "4506e5a4f4444c6db229325b3b1a2425"
-SPOTIFY_CLIENT_SECRET = "136b75613302468bac61e37f246b29c5"
+bot = None
+queues = {}
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET
-))
+def play_next_in_queue(ctx, voice_client, guild_id):
+    if queues[guild_id]:
+        source = queues[guild_id].pop(0)
+        voice_client.play(
+            source, 
+            after=lambda e: play_next_in_queue(ctx, voice_client, guild_id) if queues[guild_id] else None
+        )
+    else:
+        pass
 
-voice_clients = {}
-
-@commands.command(name="join")
+@commands.command('join')
 async def join(ctx):
     if ctx.author.voice:
         channel = ctx.author.voice.channel
-        voice_clients[ctx.guild.id] = await channel.connect()
+        if ctx.voice_client is None:
+            try:
+                await channel.connect()
+                await ctx.send(f'Joined {channel.name}!')
+            except discord.ClientException as e:
+                await ctx.send(f"Error: {e}")
+        else:
+            await ctx.send("I'm already connected to a voice channel!")
     else:
-        await ctx.send("You need to be in a voice channel to use this command.")
+        await ctx.send("You need to be in a voice channel for me to join!")
 
-@commands.command(name="leave")
+@commands.command(name='leave')
 async def leave(ctx):
-    if ctx.guild.id in voice_clients:
-        await voice_clients[ctx.guild.id].disconnect()
-        del voice_clients[ctx.guild.id]
+    if ctx.voice_client:
+        if ctx.author.voice:
+            if ctx.author.voice.channel == ctx.voice_client.channel:
+                await ctx.voice_client.disconnect()
+            else:
+                await ctx.send("You need to be in the same voice channel as me to use this command.")
+        else:
+            await ctx.send("You need to be in a voice channel to use this command.")
     else:
         await ctx.send("I'm not in a voice channel.")
 
-@commands.command(name="play")
-async def play(ctx, *, query):
-    if ctx.guild.id not in voice_clients:
+@commands.command(name='play')
+async def play(ctx, link: str):
+    if ctx.voice_channel:
         await join(ctx)
-
-    voice_client = voice_clients[ctx.guild.id]
-    
-    if "open.spotify.com" in query:
-        if "track" in query:
-            results = sp.track(query)
-            query = results['name'] + " " + results['artists'][0]['name']
-        else:
-            await ctx.send("Only Spotify track links are supported.")
-            return
 
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -55,36 +56,67 @@ async def play(ctx, *, query):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': 'song.mp3',
     }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(f"ytsearch:{query}", download=True)
-        url = info['entries'][0]['webpage_url']
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)
+        audio_url = info['url']
+        title = info.get('title', 'Unknown Title')
 
-    voice_client.stop()
-    voice_client.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: os.remove("song.mp3"))
-    await ctx.send(f"Now playing: {query}")
+    try:
+        source = discord.FFmpegOpusAudio(
+            audio_url, 
+            before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            options="-vn"
+        )
+    except Exception as e:
+        await ctx.send(f"Error creating audio source: {e}")
+        return
 
-@commands.command(name="pause")
+    guild_id = ctx.guild.id
+    if guild_id in queues:
+        queues[guild_id].append(source)
+        await ctx.send(f"Added to queue! Current rank: {len(queues[guild_id])}")
+    else:
+        queues[guild_id] = [source]
+        await ctx.send(f"Playing: {title}")
+        play_next_in_queue(ctx, ctx.voice_client, guild_id)
+
+@commands.command('pause')
 async def pause(ctx):
-    if ctx.guild.id in voice_clients and voice_clients[ctx.guild.id].is_playing():
-        voice_clients[ctx.guild.id].pause()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
     else:
-        await ctx.send("No music is currently playing.")
+        ctx.send("There is no audio playing")
 
-@commands.command(name="resume")
+@commands.command('resume')
 async def resume(ctx):
-    if ctx.guild.id in voice_clients and voice_clients[ctx.guild.id].is_paused():
-        voice_clients[ctx.guild.id].resume()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
     else:
-        await ctx.send("No music is currently paused.")
+        ctx.send("No audio is paused")
 
-# Stop the music
-@commands.command(name="stop")
-async def stop(ctx):
-    if ctx.guild.id in voice_clients:
-        voice_clients[ctx.guild.id].stop()
+@commands.command('skip')
+async def skip(ctx):
+    ctx.voice_client.stop()
+    play_next_in_queue(ctx, ctx.guild.voice_client, ctx.guild.id)
+
+@commands.command('clearPl')
+async def clearPl(ctx):
+    queues.clear()
+    ctx.voice_client.stop()
+    
+
+@commands.command(pass_content = True)
+async def record(ctx):
+    if ctx.voice_client and ctx.voice_client.is_connected():
+        ffmpeg_process = False  # need to be changed
+        if ffmpeg_process:
+            await ctx.send("Recording started! Use `!stop` to stop recording.")
+            bot.ffmpeg_process = ffmpeg_process
+        else:
+            await ctx.send("Failed to start recording.")
     else:
-        await ctx.send("No music is currently playing.")
-
+        await ctx.send("I am not connected to a voice channel!")
